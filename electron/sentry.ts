9a439c37545071ad7d7ead538d2e6cdef0a1df81
YoniRaviv <dekker320@gmail.com>
2026-03-18 09:03:58 +0200

    added sentry UUID for better observability

--- electron/sentry.ts
+++ electron/sentry.ts
@@ -1,8 +1,17 @@
 import * as Sentry from '@sentry/electron/main'
 import { app } from 'electron'
+import { randomUUID } from 'node:crypto'
 import os from 'node:os'
+import Store from 'electron-store'
 
 const SENTRY_DSN = "https://41848ed43cba7cae394e1fb388dec6ec@o4511047909179392.ingest.de.sentry.io/4511047913439312"
+
+const store = new Store<{ anonymousUserId?: string }>()
+let anonymousUserId = store.get('anonymousUserId')
+if (!anonymousUserId) {
+    anonymousUserId = randomUUID()
+    store.set('anonymousUserId', anonymousUserId)
+}
 
 Sentry.init({
     dsn: SENTRY_DSN,
@@ -18,3 +27,5 @@ Sentry.init({
         return JSON.parse(json)
     },
 })
+
+Sentry.setUser({ id: anonymousUserId })
